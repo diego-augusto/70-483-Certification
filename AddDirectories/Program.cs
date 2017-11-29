18e5d7c10c80b22b4b732cb34601cc6539ff1480
Diego Augusto <daugustomoura@gmail.com>
2017-11-28 23:14:07 -0300

    add directory example

---
+++ AddDirectories/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace AddDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory1 = Directory.CreateDirectory("Directory01");
+            DirectoryInfo directory2 = new DirectoryInfo("Directory02");
+            directory2.Create();
+        }
+    }
+}
